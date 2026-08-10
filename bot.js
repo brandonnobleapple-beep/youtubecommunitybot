726a4098d98f344d1427112cfce45285262e73c5
brandonnobleapple-beep <brandonnobleapple@gmail.com>
2026-08-10 11:02:45 -0600

    Add health server for Render Web Service

--- bot.js
+++ bot.js
@@ -5,11 +5,18 @@ const token = process.env.BOT_TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const axios = require("axios");
 const fs = require("fs");
+const http = require("http");
+
+const PORT = process.env.PORT || 10000;
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("YouTube Community Bot is running!\n");
+}).listen(PORT, "0.0.0.0", () => {
+    console.info(`Health server listening on port ${PORT}`);
+});
 
 client.once("ready", () => {
     console.info("Ready!");
-    // Get Discord Server Channel to post in
-    // TODO: Get Server from Discord.js and Channel from User Input
     const guild = client.guilds.cache.get(process.env.SERVER_ID);
     if (!guild) {
         console.error("Discord server not found. Check SERVER_ID and bot access to the server.");
@@ -22,13 +29,10 @@ client.once("ready", () => {
         return;
     }
 
-    // YT API URL for Community Posts with Channel ID
     const URL = `${process.env.YT_API_URL}${process.env.YT_CHANNEL_ID}`;
-    // Call YT API every hour
     callAPI();
     setInterval(callAPI, 3600000);
 
-    // Axios GET Request
     function callAPI() {
         axios
             .get(URL)
@@ -36,32 +40,25 @@ client.once("ready", () => {
                 let data = response.data;
                 const communityPosts = data.items[0].community;
                 let newPostID = JSON.stringify(communityPosts[0].id);
-                let lastPostID = null;
-                //console.log(communityPosts[0])
-                // Read previous YT Post ID
                 fs.readFile("./lastPostID.json", "utf8", (err, lastPostID) => {
                     if (err) {
                         console.error("File read failed:", err);
                         return;
-                    } else {
-                        lastPostID = JSON.parse(lastPostID);
-                        newPostID = JSON.parse(newPostID);
-                        console.info(`ID of previous post is: ${lastPostID}`);
-                        console.info(`ID of latest post is: ${newPostID}`);
+                    }
+                    lastPostID = JSON.parse(lastPostID);
+                    newPostID = JSON.parse(newPostID);
+                    console.info(`ID of previous post is: ${lastPostID}`);
+                    console.info(`ID of latest post is: ${newPostID}`);
 
-                        if (lastPostID === newPostID) {
-                            console.info("No new posts");
-                        } else {
-                            // Find previous post array index and slice the array to get newer posts
-                            communityPosts.forEach((post, index) => {
-                                console.info(lastPostID, post.id);
-                                if (post.id === lastPostID) {
-                                    console.info(index);
-                                    const newPosts = communityPosts.slice(0, index);
-                                    postContent(newPosts, newPostID);
-                                }
-                            });
-                        }
+                    if (lastPostID === newPostID) {
+                        console.info("No new posts");
+                    } else {
+                        communityPosts.forEach((post, index) => {
+                            if (post.id === lastPostID) {
+                                const newPosts = communityPosts.slice(0, index);
+                                postContent(newPosts, newPostID);
+                            }
+                        });
                     }
                 });
             })
@@ -70,57 +67,35 @@ client.once("ready", () => {
             });
     }
 
-    // Format and send Discord message to channel
     function postContent(newPosts, newPostID) {
         newPosts.forEach((post) => {
             const postText = post.contentText[0].text;
-            console.info(postText);
             if (post.image && post.image.thumbnails[5]) {
                 const imgURL = post.image.thumbnails[5].url;
                 const imgEmbed = new EmbedBuilder()
                     .setTitle("New YT Image")
                     .setDescription(`**Description:** ${postText} \n**Post Link:** https://www.youtube.com/post/${post.id}`)
                     .setImage(imgURL);
-                channel.send({
-                    embeds: [imgEmbed],
-                })
-                    .then(console.log)
-                    .catch(console.error);
+                channel.send({ embeds: [imgEmbed] }).catch(console.error);
             } else if (post.poll) {
                 let choiceArray = [];
-                post.poll.choices.forEach((choice) => {
-                    choiceArray.push(choice.text);
-                });
+                post.poll.choices.forEach((choice) => choiceArray.push(choice.text));
                 choiceArray = choiceArray.join("\n");
                 const pollEmbed = new EmbedBuilder()
                     .setTitle("New YT Poll")
-                    .setDescription(
-                        `**Poll Title:** ${postText} \n**Choices:** \n${choiceArray}\n**Total Votes:** ${post.poll.totalVotes} \n**Poll Link:** https://www.youtube.com/post/${post.id}`
-                    );
-                channel.send({
-                    embeds: [pollEmbed],
-                })
-                    .then(console.log)
-                    .catch(console.error);
+                    .setDescription(`**Poll Title:** ${postText} \n**Choices:** \n${choiceArray}\n**Total Votes:** ${post.poll.totalVotes} \n**Poll Link:** https://www.youtube.com/post/${post.id}`);
+                channel.send({ embeds: [pollEmbed] }).catch(console.error);
             } else {
                 const textEmbed = new EmbedBuilder()
                     .setTitle("New YT Post")
                     .setDescription(`**Post Text:** ${postText} \n**Post Link:** https://www.youtube.com/post/${post.id}`);
-                channel.send({
-                    embeds: [textEmbed],
-                })
-                    .then(console.log)
-                    .catch(console.error);
+                channel.send({ embeds: [textEmbed] }).catch(console.error);
             }
         });
-        // Write the new post ID to the JSON file
-        newPostID = JSON.stringify(newPostID);
-        fs.writeFile("lastPostID.json", newPostID, function (err) {
-            if (err) {
-                console.info(err);
-            } else {
-                console.info("ID written");
-            }
+
+        fs.writeFile("lastPostID.json", JSON.stringify(newPostID), function (err) {
+            if (err) console.info(err);
+            else console.info("ID written");
         });
     }
 });
