cd842bc811a4d915fdefdf2576ce4c94c867500d
brandonnobleapple-beep <brandonnobleapple@gmail.com>
2026-08-10 10:18:47 -0600

    Fix Discord channel lookup in bot.js

--- bot.js
+++ bot.js
@@ -10,9 +10,18 @@ client.once("ready", () => {
     console.info("Ready!");
     // Get Discord Server Channel to post in
     // TODO: Get Server from Discord.js and Channel from User Input
-    const channel = client.guilds.cache
-        .get(process.env.SERVER_ID)
-        .client.channels.cache.get(process.env.CHANNEL_ID);
+    const guild = client.guilds.cache.get(process.env.SERVER_ID);
+    if (!guild) {
+        console.error("Discord server not found. Check SERVER_ID and bot access to the server.");
+        return;
+    }
+
+    const channel = guild.channels.cache.get(process.env.CHANNEL_ID);
+    if (!channel) {
+        console.error("Discord channel not found. Check CHANNEL_ID and bot access to the channel.");
+        return;
+    }
+
     // YT API URL for Community Posts with Channel ID
     const URL = `${process.env.YT_API_URL}${process.env.YT_CHANNEL_ID}`;
     // Call YT API every hour
@@ -24,11 +33,11 @@ client.once("ready", () => {
         axios
             .get(URL)
             .then(function (response) {
-		let data = response.data;
+                let data = response.data;
                 const communityPosts = data.items[0].community;
                 let newPostID = JSON.stringify(communityPosts[0].id);
                 let lastPostID = null;
-		//console.log(communityPosts[0])
+                //console.log(communityPosts[0])
                 // Read previous YT Post ID
                 fs.readFile("./lastPostID.json", "utf8", (err, lastPostID) => {
                     if (err) {
@@ -40,20 +49,19 @@ client.once("ready", () => {
                         console.info(`ID of previous post is: ${lastPostID}`);
                         console.info(`ID of latest post is: ${newPostID}`);
 
-						if (lastPostID === newPostID) {
-							console.info("No new posts");
-						} else {
-						
-                        // Find previous post array index and slice the array to get newer posts
-                        communityPosts.forEach((post, index) => {
-                        console.info(lastPostID, post.id);
-                            if (post.id === lastPostID) {
-                                console.info(index);
-                                const newPosts = communityPosts.slice(0, index);
-                                postContent(newPosts, newPostID);
-                            }
-                        });
-                    }
+                        if (lastPostID === newPostID) {
+                            console.info("No new posts");
+                        } else {
+                            // Find previous post array index and slice the array to get newer posts
+                            communityPosts.forEach((post, index) => {
+                                console.info(lastPostID, post.id);
+                                if (post.id === lastPostID) {
+                                    console.info(index);
+                                    const newPosts = communityPosts.slice(0, index);
+                                    postContent(newPosts, newPostID);
+                                }
+                            });
+                        }
                     }
                 });
             })
@@ -76,8 +84,8 @@ client.once("ready", () => {
                 channel.send({
                     embeds: [imgEmbed],
                 })
-                  .then(console.log)
-                  .catch(console.error);
+                    .then(console.log)
+                    .catch(console.error);
             } else if (post.poll) {
                 let choiceArray = [];
                 post.poll.choices.forEach((choice) => {
@@ -92,8 +100,8 @@ client.once("ready", () => {
                 channel.send({
                     embeds: [pollEmbed],
                 })
-                  .then(console.log)
-                  .catch(console.error);
+                    .then(console.log)
+                    .catch(console.error);
             } else {
                 const textEmbed = new EmbedBuilder()
                     .setTitle("New YT Post")
@@ -101,8 +109,8 @@ client.once("ready", () => {
                 channel.send({
                     embeds: [textEmbed],
                 })
-                  .then(console.log)
-                  .catch(console.error);
+                    .then(console.log)
+                    .catch(console.error);
             }
         });
         // Write the new post ID to the JSON file
